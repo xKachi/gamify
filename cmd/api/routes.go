52a8954a3594dc32bf1baeea1c4616d839aa9b35
xKachi <kindtheideator@gmail.com>
2025-04-19 17:39:06 +0100

    Parsing JSON requests

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,3 +1,11 @@
+/*
+Routes handles the handlers
+	Notfound - When a requested handler is ot available
+	MethonotAllowed - When the method is not allowed on the handler
+Handlers handles the requests to the server
+	Notfound - When the requested response is not found
+*/
+
 package main
 
 import (
@@ -9,9 +17,17 @@ import (
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
+	// Convert the notFoundResponse() helper to a http.Handler using the
+	// http.HandlerFunc() adapter, and then set it as the custom error handler for 404
+	// Not Found responses.
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	// Likewise, convert the methodNotAllowedResponse() helper to a http.Handler and set
+	// it as the custom error handler for 405 Method Not Allowed responses.
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/games", app.createGameHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/games/:id", app.showGameHandler)
 
-	return router
+	return app.recoverPanic(router)
 }
